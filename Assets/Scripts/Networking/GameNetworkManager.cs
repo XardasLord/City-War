a76bfd99ce4a042f0e5719959f615fb9ddb8b45a
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-12-05 18:25:49 +0100

    Implemented base NetworkManager and player prefabs

---
+++ Assets/Scripts/Networking/GameNetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Networking
+{
+    public class GameNetworkManager : NetworkManager
+    {
+        public override void OnServerAddPlayer(NetworkConnection conn)
+        {
+            base.OnServerAddPlayer(conn);
+
+            Debug.Log($"Player with ID {conn.connectionId} connected to the server! Active numbers of players on the server: {numPlayers}");
+        }
+    }
+}
