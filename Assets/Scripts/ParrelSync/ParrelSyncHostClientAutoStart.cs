ae5f6ca34595d528e64457ea85a80977c8c086bf
Paweł Kowalewicz <kowalewicz.pawel@gmail.com>
2020-12-30 13:39:30 +0100

    ParrelSync configuration to auto start host and client

---
+++ Assets/Scripts/ParrelSync/ParrelSyncHostClientAutoStart.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_EDITOR
+using Mirror;
+using UnityEngine;
+
+namespace ParrelSync
+{
+    public class ParrelSyncHostClientAutoStart : MonoBehaviour
+    {
+        private void Start()
+        {
+            var networkManager = GetComponent<NetworkManager>();
+            networkManager.networkAddress = "localhost";
+
+            if (ClonesManager.IsClone())
+                networkManager.StartClient();
+            else
+                networkManager.StartHost();
+        }
+    }
+}
+#endif
